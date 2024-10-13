c144bd99c2522616f86fab3c562cb088c971179e
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2024-10-12 21:17:50 -0300

    feat: dabase mock

---
+++ data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 1,
+        dsc_titulo: 'Lavar pratos',
+        dsc_descricao: 'Manter casa organizada lavando pratos',
+        tipo: 'diario'
+    },
+    {
+        id: 2,
+        dsc_titulo: 'Arrumar a cama',
+        dsc_descricao: 'Organizar o quarto ao acordar',
+        tipo: 'diario'
+    },
+    {
+        id: 3,
+        dsc_titulo: 'Fazer compras',
+        dsc_descricao: 'Comprar alimentos e produtos essenciais',
+        tipo: 'semanal'
+    },
+    {
+        id: 4,
+        dsc_titulo: 'Exercitar-se',
+        dsc_descricao: 'Praticar atividades físicas por 30 minutos',
+        tipo: 'diario'
+    },
+    {
+        id: 5,
+        dsc_titulo: 'Limpar o banheiro',
+        dsc_descricao: 'Higienizar o banheiro para evitar sujeira',
+        tipo: 'semanal'
+    },
+    {
+        id: 6,
+        dsc_titulo: 'Estudar programação',
+        dsc_descricao: 'Aprimorar habilidades com exercícios e projetos',
+        tipo: 'diario'
+    }
+];
